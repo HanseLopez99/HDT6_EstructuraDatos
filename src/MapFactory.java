d44c3a2ba69af648e0f2e6b7d8efd1ad4043d1db
eduardorh1312 <53280540+eduardorh1312@users.noreply.github.com>
2020-03-09 16:45:42 -0600

    Factory Creado, revisar

---
+++ src/MapFactory.java
@@ -0,0 +1,24 @@
+public class MapFactory{
+
+	public Map getMap(String mapType){
+
+		if(mapType == null){
+			return null;
+		}
+
+		if(mapType.equalsIgnoreCase("HashMap")){
+			return new HashMap();
+		}
+
+		if(mapType.equalsIgnoreCase("TreeMap")){
+			return new TreeMap();
+		}
+
+		if(mapType.equalsIgnoreCase("LinkedHashMap")){
+			return new LinkedHashMap();
+		}
+
+		return Map();
+
+	}
+}

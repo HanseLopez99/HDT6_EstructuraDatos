18f543321d2a213d3ed4812b00bc106ce4f9bebb
eduardorh1312 <53280540+eduardorh1312@users.noreply.github.com>
2020-03-10 17:41:37 -0600

    clase para lector de archivos

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.io.*;
 /*
 * @author: Hansel Lopez & Eduardo Ramírez
 *	Hoja de Trabajo #6
@@ -9,33 +8,8 @@ public class Main{
 
     public static void main(String [] args) {
 
-        String fileName = "cards_desc.txt";
+        fileRead f = new fileRead();
 
-        String line = null;
-
-        try {
-
-            FileReader fileReader = 
-                new FileReader(fileName);
-
-            BufferedReader bufferedReader = 
-                new BufferedReader(fileReader);
-
-            while((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
-            }   
-
-            bufferedReader.close();         
-        }
-        catch(FileNotFoundException ex) {
-            System.out.println(
-                "Unable to open file '" + 
-                fileName + "'");                
-        }
-        catch(IOException ex) {
-            System.out.println(
-                "Error reading file '" 
-                + fileName + "'");                  
-        }
+        f.Reader();
     }
 }
---
+++ src/fileRead.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class fileRead{
+
+	public void Reader(){
+
+		String fileName = "cards_desc.txt";
+
+        String line = null;
+
+        try {
+
+            FileReader fileReader = 
+                new FileReader(fileName);
+
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }   
+
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+        }
+    }
+
+}

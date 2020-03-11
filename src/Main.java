2e871133b0b79ae3501b8b890511021b18d83406
HanseLopez99 <halm29799@gmail.com>
2020-03-10 19:57:46 -0600

    Ligeras correcciones en patron Factory

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,16 @@
 *	Algoritmos y estructuras de datos //lectura de archivo .txt
 */
 
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.TreeMap;
+
 
 public class Main{
 
     public static void main(String [] args) {
 
-    	Map<String, String> map;
-    	Map<String, String> treeMap;
-    	Map<String, String> linkedHashMap;
+    	MapFactory factory = new MapFactory();
     	
         fileRead f = new fileRead();
         f.Reader();

ec177495cf9c6fb74a8d48f18f8a57920b955f6a
HanseLopez99 <halm29799@gmail.com>
2020-03-10 23:51:44 -0600

    Se separo nombre y tipo en dos arraylist

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,15 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 
 public class Main{
 
     public static void main(String [] args) {
-
+		ArrayList<String> nombresCartas = new ArrayList<String>();
+		ArrayList<String> tipos = new ArrayList<String>();
+		Map<String, String> map;
     	Scanner sc = new Scanner(System.in);
     	String input;
     	MapFactory factory = new MapFactory();
@@ -25,11 +28,15 @@ public class Main{
     	System.out.println("________________________________________________________--\n");
     	System.out.println("Que implementacion de MAP desea utilizar: \n");
     	System.out.println("Escribir: 'HashMap', 'TreeMap' o 'LinkedHashMap'");
-    	
+		
     	input = sc.nextLine();
-    	Map<String, String> map = factory.getMap(input);
-    	file.Reader();
-        
+		map = factory.getMap(input);
+    	
+		
+		nombresCartas = file.Reader();
+		tipos = file.Reader2();
+		System.out.println(tipos);
+
 
         System.out.println("\n -- Fin de la ejecucion...");
 

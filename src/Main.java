5cf628a41f9b675b703875ce52bc7b10a8c0a6b4
HanseLopez99 <halm29799@gmail.com>
2020-03-10 19:18:46 -0600

    Importaciones necesarias

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,24 @@
 *	Algoritmos y estructuras de datos //lectura de archivo .txt
 */
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
 public class Main{
 
     public static void main(String [] args) {
 
+    	Map<Integer, String> map = new HashMap<Integer, String>();
+    	Map<Integer, String> treeMap = new TreeMap<Integer, String>();
+    	Map<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
+    	
         fileRead f = new fileRead();
-
         f.Reader();
+        
+        
     }
 }
